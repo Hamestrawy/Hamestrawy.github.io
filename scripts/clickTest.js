d5bf0913d4490b508c713d7607b974afd3ca937c
OHMe <196162502@qq.com>
2024-09-15 19:46:43 +0800

    update

--- scripts/clickTest.js
+++ scripts/clickTest.js
@@ -3,6 +3,7 @@ const bigButton = document.getElementById("bigButton");
 let clickCount = 0;
 const countDisplay = document.getElementById("countDisplay");
 const countList = document.getElementById("countList");
+const seconds = document.getElementById('seconds');
 
 function startTest() {
     clickCount ++;
@@ -10,14 +11,23 @@ function startTest() {
     if(testing) return;
     testing = true;
     bigButton.classList.add("big-button-full");
-    setTimeout('endTest()', 1000);
+    setTimeout('endTest()', parseInt(seconds.value) * 1000);
 }
 
 function endTest() {
     let newCount = document.createElement("li");
-    newCount.textContent = clickCount.toString();
+    newCount.textContent = clickCount.toString() + ' times in ' + seconds.value + ' seconds';
     countList.appendChild(newCount);
     clickCount = 0;
     bigButton.classList.remove("big-button-full");
     testing = false;
+}
+
+function checkSeconds() {
+    let value = seconds.value.replace(/0-9/g, '').trim()
+    if (value === '') {
+        value = '1'
+    }
+    value = Math.max(1, parseInt(value))
+    seconds.value = value.toString()
 }
